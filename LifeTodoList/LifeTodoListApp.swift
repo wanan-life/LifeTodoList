4dd9a4a4c4e5d82c4c4802fe4e7a2363f713cca1
wanan-life <hackwanan@gmail.com>
2026-01-16 00:49:34 +0800

    Initial Commit

---
+++ LifeTodoList/LifeTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeTodoListApp.swift
+//  LifeTodoList
+//
+//  Created by wanan on 2026/1/16.
+//
+
+import SwiftUI
+
+@main
+struct LifeTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
